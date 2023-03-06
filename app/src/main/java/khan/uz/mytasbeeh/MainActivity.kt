69c8b3fda93b256a5ad4237e7bd1e1cd2327c0b6
Shibo14 <samsung98abbos@gmail.com>
2023-03-06 11:43:34 +0500

    button while

--- app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
+++ app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
@@ -1,19 +1,21 @@
 package khan.uz.mytasbeeh
 
 import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
+import androidx.appcompat.app.AppCompatActivity
 import khan.uz.mytasbeeh.databinding.ActivityMainBinding
 
 @Suppress("LABEL_NAME_CLASH")
 class MainActivity : AppCompatActivity() {
 
-
+    private var BgMusic:MediaPlayer? = null
     private var count: Int = 0
     private lateinit var binding: ActivityMainBinding
     private lateinit var mediaPlayer: MediaPlayer
+    private var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,33 +23,56 @@ class MainActivity : AppCompatActivity() {
         binding.imageView2.setImageResource(R.drawable.music_note_24)
         binding.res.setImageResource(R.drawable.ic_baseline_refresh_24)
         val vibe: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.mixkit)
         binding.button.setOnClickListener {
+
             count++
             vibe.vibrate(200)
-            mediaPlayer = MediaPlayer.create(this, R.raw.mixkit)
+
+    mediaPlayer.start()
+
             binding.progressBar.progress = count
             binding.textView.text = count.toString()
 
 
-            mediaPlayer.start()
-
         }
 
-        binding.res.setOnClickListener {
-            count = 0
-            binding.textView.text = count.toString()
-        }
+
+
 
         binding.imageView2.setOnClickListener {
 
+            i++
 
-            binding.imageView2.isSoundEffectsEnabled = false
-            binding.imageView2.setImageResource(R.drawable.music_off_24)
+            if (i == 1) {
+           mediaPlayer.pause()
+                val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
+                amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true)
+                binding.imageView2.setImageResource(R.drawable.music_off_24)
+            } else if (i == 2) {
+                val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
+                amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false)
+                binding.imageView2.setImageResource(R.drawable.music_note_24)
+                i = 0
+            }
+
 
         }
 
 
+    binding.res.setOnClickListener{
+
+        count = 0
+        binding.progressBar.progress = count
+        binding.textView.text = count.toString()
+
     }
+
+
+}
+
+
 
 
 }
