0e8961e7ba323023543683e96ab754fa4b4d7d98
Shibo14 <samsung98abbos@gmail.com>
2023-03-06 21:07:09 +0500

    button

--- app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
+++ app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
@@ -11,7 +11,7 @@ import khan.uz.mytasbeeh.databinding.ActivityMainBinding
 @Suppress("LABEL_NAME_CLASH")
 class MainActivity : AppCompatActivity() {
 
-    private var BgMusic:MediaPlayer? = null
+
     private var count: Int = 0
     private lateinit var binding: ActivityMainBinding
     private lateinit var mediaPlayer: MediaPlayer
@@ -28,15 +28,16 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
 
             count++
-            vibe.vibrate(200)
+            vibe.vibrate(100)
 
-    mediaPlayer.start()
+
 
             binding.progressBar.progress = count
             binding.textView.text = count.toString()
 
-
+            mediaPlayer.start()
         }
+
 
 
 
@@ -46,11 +47,13 @@ class MainActivity : AppCompatActivity() {
             i++
 
             if (i == 1) {
-           mediaPlayer.pause()
+                mediaPlayer.setVolume(0.0F, 0.0F)
+                mediaPlayer.pause()
                 val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
                 amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true)
                 binding.imageView2.setImageResource(R.drawable.music_off_24)
             } else if (i == 2) {
+                mediaPlayer.setVolume(1F, 1F);
                 val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
                 amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false)
                 binding.imageView2.setImageResource(R.drawable.music_note_24)
@@ -61,18 +64,25 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-    binding.res.setOnClickListener{
+        binding.res.setOnClickListener {
 
-        count = 0
-        binding.progressBar.progress = count
-        binding.textView.text = count.toString()
+            count = 0
+            binding.progressBar.progress = count
+            binding.textView.text = count.toString()
+
+        }
+
 
     }
 
+    fun music() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.mixkit)
 
-}
+        if (mediaPlayer.isPlaying) {
 
+        }
 
+    }
 
 
 }
