50fdd83ac9775dc30a74747a675c3902a1634c6b
Shibo14 <samsung98abbos@gmail.com>
2023-03-12 08:40:27 +0500

    update

--- app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
+++ app/src/main/java/khan/uz/mytasbeeh/MainActivity.kt
@@ -1,8 +1,10 @@
 package khan.uz.mytasbeeh
 
 import android.content.Context
-import android.media.AudioManager
+import android.media.AudioAttributes
+
 import android.media.MediaPlayer
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
@@ -16,47 +18,77 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mediaPlayer: MediaPlayer
     private var i = 0
+    private var soundPool: SoundPool? = null
+    private var soundPlayer: Int = 0
+    private var isTap = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.imageView2.setImageResource(R.drawable.music_note_24)
+
+        binding.btnSound.setImageResource(R.drawable.music_note_24)
+        binding.vibrationBtn.setImageResource(R.drawable.noun_vibrate_on)
         binding.res.setImageResource(R.drawable.ic_baseline_refresh_24)
+
         val vibe: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.mixkit)
+        val audioAttributes = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build()
+
+        soundPool = SoundPool.Builder().setMaxStreams(2).setAudioAttributes(audioAttributes).build()
+
+
+        soundPlayer = soundPool!!.load(this, R.raw.tap, 1)
+
         binding.button.setOnClickListener {
 
+            if (isTap) {
+                soundPool?.play(soundPlayer, 1f, 1f, 1, 0, 1f)
+            }
+               if (isTap) {
+                   vibe.vibrate(100)
+            }
+
             count++
-            vibe.vibrate(100)
-
-
 
             binding.progressBar.progress = count
             binding.textView.text = count.toString()
 
-            mediaPlayer.start()
+
         }
 
 
-
-
-
-        binding.imageView2.setOnClickListener {
+        binding.btnSound.setOnClickListener {
 
             i++
 
             if (i == 1) {
-                mediaPlayer.setVolume(0.0F, 0.0F)
-                mediaPlayer.pause()
-                val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
-                amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true)
-                binding.imageView2.setImageResource(R.drawable.music_off_24)
+                isTap =false
+                soundPool?.stop(soundPlayer)
+
+
+                binding.btnSound.setImageResource(R.drawable.music_off_24)
             } else if (i == 2) {
-                mediaPlayer.setVolume(1F, 1F);
-                val amanager = getSystemService(AUDIO_SERVICE) as AudioManager
-                amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false)
-                binding.imageView2.setImageResource(R.drawable.music_note_24)
+
+
+                isTap = true
+                binding.btnSound.setImageResource(R.drawable.music_note_24)
+                i = 0
+            }
+
+
+        }
+        binding.vibrationBtn.setOnClickListener {
+
+            i++
+
+            if (i == 1) {
+                isTap =false
+               vibe.vibrate(0)
+                binding.vibrationBtn.setImageResource(R.drawable.noun_vibration_off)
+            } else if (i == 2) {
+                isTap = true
+                binding.vibrationBtn.setImageResource(R.drawable.noun_vibrate_on)
                 i = 0
             }
 
